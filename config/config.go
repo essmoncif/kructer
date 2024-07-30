ddd26f82645491059c9c56a7e5f5e0ab492846be
moncif <moncefessaoudi@gmail.com>
2024-07-30 14:37:47 +0100

    add config & db connection

---
+++ config/config.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"github.com/caarlos0/env"
+	"github.com/joho/godotenv"
+)
+
+type Configuration struct {
+	Address          string `env:"ADDRESS" envDefault:":8080"`
+	ConnectionString string `env:"CONNECTION_STRING,required"`
+}
+
+func New(files ...string) (*Configuration, error) {
+	if err := godotenv.Load(files...); err != nil {
+		panic("No env file could be found")
+	}
+
+	conf := Configuration{}
+	if err := env.Parse(&conf); err != nil {
+		return nil, err
+	}
+
+	return &conf, nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module kructer.com
 
 go 1.22.5
+
+require (
+	github.com/caarlos0/env v3.5.0+incompatible // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
